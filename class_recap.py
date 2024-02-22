dc1078485b79c72eebce1d5a26c91152c4432a4d
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-22 20:06:44 +0900

    AttributeError 'NoneType object has no attribute 'damage'

---
+++ class_recap.py
@@ -0,0 +1,21 @@
+def Monster():
+    hp = 100
+    alive = True
+    
+    def damage(self, hp):
+        self.hp = self.hp - hp
+        if self.hp < 100:
+            alive = False
+        else:
+            alive = True
+
+    def check_status():
+        if alive == True:
+            print("alive!")
+        else:
+            print("Died")
+
+monster1 = Monster()
+monster2 = Monster()
+
+monster1.damage() # AttributeError: 'NoneType' object has no attribute 'damage'
