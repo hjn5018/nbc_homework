30462d8a64f6f6304a256c8384b3c6723c2afe9c
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-22 21:44:07 +0900

    주석 작성, class에 괄호 사용 질문 1st line #

--- sns_prac.py
+++ sns_prac.py
@@ -1,11 +1,11 @@
-class Member:
+class Member():                # ()괄호를 사용해도 실행이 되고, 괄호를 사용하지 않아도 실행이 되는데, 어떤 기능이 있나요?
     name = ""
     username = ""
     password = ""
     def display(self):
         print(self.name, self.username)
 
-class Post:
+class Post():
     title = ""
     content = ""
     author = "" # !!! username이 저장되어야 함!!!
@@ -14,16 +14,26 @@ class Post:
 # member2 = Member("카다")
 # member3 = Member("보")
 
-member1 = Member()
+member1 = Member()            # 인스턴스 생성
 member2 = Member()
 member3 = Member()
 
+member1.name = '몰리1'        # 인스턴스 속성 부여
+member1.username = '카다1'
+member1.password = '보1'
 
-member1.name = '몰리'
-member1.username = '카다'
-member1.password = '보'
+member2.name = '몰리2'
+member2.username = '카다2'
+member2.password = '보2'
 
-member1.display()
+member3.name = '몰리3'
+member3.username = '카다3'
+member3.password = '보3'
+
+
+member1.display()            # display()메소드 사용
+member2.display()
+member3.display()
 
 members = []
 # print(type(members))
