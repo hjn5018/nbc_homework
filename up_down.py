e1f76b19e6d4744b571f0d7fd6c2ab59eb126fbb
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-22 19:09:20 +0900

    추가 도전 과제 달성 필요

--- up_down.py
+++ up_down.py
@@ -1,23 +1,37 @@
 import random
 
 random_number = random.randint(1, 100) # 1 ~ 100 무작위 숫자 선정
-print(random_number)
+print(random_number) # 테스트 용도
 
-player_input = input() # 사용자 입력
+print("<<업 다운 게임을 시작합니다.>>")
 
-# if player_input > random_number:
+# if player_input == random_number: # 정답을 맞출 경우 "정답입니다!" 출력
+#     print("정답입니다!")
+# elif player_input > random_number: # 사용자 입력값이 정답보다 클 경우 "다운" 출력
 #     print("다운")
-# else:
-#     print("ㄱㄷㄱㄷ")
-#     # TypeError: '>' not supported between instances of 'str' and 'int'
+# else: # 나머지 경우 "업" 출력
+#     print("업")
 
-player_input = int(player_input) # 사용자 입력값 int로 변환
+i = 1 # 게임 시행 횟수
 
-if player_input == random_number: # 정답을 맞출 경우 "정답입니다!" 출력
-    print("정답입니다!")
-elif player_input > random_number: # 사용자 입력값이 정답보다 클 경우 "다운" 출력
-    print("다운")
-else: # 나머지 경우 "업" 출력
-    print("업")
+while True:
+    i += 1
+    player_input = int(input("숫자를 입력하세요: ")) # player 입력 // str -> int
+    
+    if player_input > random_number:
+        print("다운")
+        
+    elif player_input < random_number:
+        print("업")
+        
+    else:
+        print("정답입니다.")
+        print(f"시도한 횟수: {i}")
+        break
 
-while player_input != random_number:
+# 풀이과정
+# if문으로 골격 만들기(8th line ~ 13rd line)
+# for문은 도저히 안 될 것 같아서, 아직 배우지 않은 while문 사용
+# break는 떠올랐는데 continue가 떠오르지 않았음
+# continue 떠오르고 적용했는데, 무한루프 발생(탈출 : Ctrl+C)
+# continue 제외하고 break만 사용
