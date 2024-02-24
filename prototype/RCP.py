05ff7bc8991b8cb2aaeb39586d7bd00fd913c9af
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-24 11:22:04 +0900

    승무패 기록 안내 함수 적용!

--- prototype/RCP.py
+++ prototype/RCP.py
@@ -42,39 +42,78 @@ print("가위 바위 보 게임을 시작합니다!")
 #     else:
 #         print("비겼습니다.")
 # =========================경우의 수=====================================
+records = [] # 게임 승무패 기록 저장하기위한 빈 리스트 생성
+
+def winlosedraw(list):                      # 승무패 기록 출력 함수 .ver2(인덴트 수정 win_num부터 print()까지)
+    win_list = []
+    lose_list = []
+    draw_list = []
+    for i in list:
+        if i == "승":
+            win_list.append(i)
+        elif i == "패":
+            lose_list.append(i)
+        else:
+            draw_list.append(i)
+
+    win_num = len(win_list)
+    lose_num = len(lose_list)
+    draw_num = len(draw_list)
+
+    print(f"승 : {win_num}, 패 : {lose_num}, 무승부 : {draw_num}")
 
 while True:
     player_input = input("안 내면 진거 가위 바위 보!: ")  # player가 가위, 바위, 보 중 하나를 입력한다.
 
     if player_input == "가위":
         if computer_RCP == "가위":
+            records.append("무")      # 게임 결과 records 리스트에 기록하기
             print("비겼습니다.")       # 비긴 경우 player에게 알려주기
         elif computer_RCP == "바위":
+            records.append("패")
             print("졌습니다..")        # player가 진 경우 player에게 알려주기
         else:                         # computer__input == "보" 인 경우
+            records.append("승")
             print("이겼습니다!")       # player가 이긴 경우 player에게 알려주기
     elif player_input == "바위":
         if computer_RCP == "가위":
+            records.append("승")
             print("이겼습니다!")
         elif computer_RCP == "바위":
+            records.append("무")
             print("비겼습니다.")
         else:
+            records.append("패")
             print("졌습니다..")
     else:                             # player_input == "보" 인 경우
         if computer_RCP == "가위":
+            records.append("패")
             print("졌습니다..")
         elif computer_RCP == "바위":
+            records.append("승")
             print("이겼습니다!")
         else:
+            records.append("무")
             print("비겼습니다.")
 
     ask_more = input("한 판 더? (y/n): ")  # 게임 반복 또는 종료
     if ask_more == "y":
         continue
     else:
+        winlosedraw(records)
+        
+        # 승 : 1, 패 : 0, 무승부 : 0
+        # 승 : 1, 패 : 1, 무승부 : 0
+        # 승 : 1, 패 : 1, 무승부 : 1
+        # 승 : 2, 패 : 1, 무승부 : 1
+        # 이게 반복이 되네;;
+        # TypeError: winlosedraw() missing 1 required positional argument: 'list'
+        # 리스트(records)를 인수로 넣자!
+        # print(records)              # 게임 기록 저장 확인 완료
+        # print(f"전적 : {}")          # player에게 게임 기록 알려주기 // {}에 변수 넣어야함!!
         break
 
-
+# ===============================================================================================
 # 풀이과정
 # 가위바위보를 하긴 해야하니까
 # 1. 컴퓨터도 주먹을 쥐어준다.
@@ -87,9 +126,58 @@ while True:
 # 8. if문은 처리했고, 반복문을 넣어야 하는데..
 #    여기서도 for문은 힘들 것 같아서 while을 적용한다.
 # 9. 반복or종료 여부를 묻고(input)(y/n) continue와 break로 마감한다. 
+# 10. 승무패 기록 만들기 (빈 리스트, 분기마다 append, 함수로 승무패 각각의 리스트 생성[for문 append, len()])
 # =========================================================
 #     **추가 도전 과제:**
 
 # 1. 게임의 승, 패, 무승부 횟수를 기록하고, 게임 종료 시에 플레이어에게 통계를 제공하세요.
 # 2. 플레이어가 입력할 때 대소문자를 구분하지 않도록 프로그램을 개선하세요.
 # 3. 플레이어가 게임을 반복하고 싶을 경우, 게임 재시작 여부를 묻고 그에 따라 게임을 초기화하거나 종료하는 기능을 추가하세요.
+
+
+# ================================================================================
+
+# 승무패 기록 저장하고
+# (승 : x 패 : y 무승부 : z)로 나타내려면??
+# 일단 append했으니까 그거부터 확인하자. -> 확인 완료(else문에서 print(records))
+    
+# 이제 list에서 승무패를 갈라야하는데 if문??
+# =======================================================================================
+# def winlosedraw(list):                      # 승무패 기록 출력 함수 .ver1
+#     win_list = []
+#     lose_list = []
+#     draw_list = []
+#     for i in list:
+#         if i == "승":
+#             win_list.append(i)
+#         elif i == "패":
+#             lose_list.append(i)
+#         else:
+#             draw_list.append(i)
+
+#         win_num = len(win_list)
+#         lose_num = len(lose_list)
+#         draw_num = len(draw_list)
+
+#         print(f"승 : {win_num}, 패 : {lose_num}, 무승부 : {draw_num}")
+
+# 인덴트 때문에 for문에 print가 들어가버렸네;;
+# ===========================================================================================
+# def winlosedraw(list):                      # 승무패 기록 출력 함수 .ver2(인덴트 수정 win_num부터 print()까지)
+#     win_list = []
+#     lose_list = []
+#     draw_list = []
+#     for i in list:
+#         if i == "승":
+#             win_list.append(i)
+#         elif i == "패":
+#             lose_list.append(i)
+#         else:
+#             draw_list.append(i)
+
+#     win_num = len(win_list)
+#     lose_num = len(lose_list)
+#     draw_num = len(draw_list)
+
+#     print(f"승 : {win_num}, 패 : {lose_num}, 무승부 : {draw_num}")
+# 잘 되네 ^^
