92795d20a4a33d7ffe169f846ef40fd654c1022c
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-27 18:31:12 +0900

    생성자(__init__)적용 실험, 리스트와 딕셔너리 도입 실험

--- sns_rebuilding.py
+++ sns_rebuilding.py
@@ -50,32 +50,70 @@
 # m1 = Member("도노반", "Mr.70", 70)
 # # TypeError: __init__() takes 1 positional argument but 4 were given
 
+# class Member:
+#     def __init__(self, name, username, password):
+#         self.name = name
+#         self.username = username
+#         self.password = password
+
+# m1 = Member("도노반", "Mr.70", 70)
+# # TypeError: __init__() takes 1 positional argument but 4 were given
+
+# # print(m1)
+# # # <__main__.Member object at 0x000001874AADA790>
+
+# # print(m1.name)
+# # # 도노반
+
+# # print(m1.name, m1.username)
+# # # 도노반 Mr.70
+
+
+# user_dict = {}
+
+# class Member:
+
+#     def __init__(self, name, username, password):
+#         self.name = name
+#         self.username = username
+#         self.password = password
+#         # user_list.append(name)
+#         user_dict[name] = self.name
+#         # print(self.name, self.username)
+#     def user_info(self):
+#         print(self.name, self.username)
+
+
+# m1 = Member("도노반", "Mr.70", 70)
+# m2 = Member("스코티", "by", "rapgod")
+# m3 = Member("불곰", "콜라", "주세요")
+
+# # m1.user_info()
+# # # 도노반 Mr.70
+
+# print(user_dict)
+# {'name': '불곰'}
+
+user_list = []
+user_dict = {}
+
 class Member:
     def __init__(self, name, username, password):
         self.name = name
         self.username = username
         self.password = password
-
-m1 = Member("도노반", "Mr.70", 70)
-# TypeError: __init__() takes 1 positional argument but 4 were given
-
-# print(m1)
-# # <__main__.Member object at 0x000001874AADA790>
-
-# print(m1.name)
-# # 도노반
-
-# print(m1.name, m1.username)
-# # 도노반 Mr.70
-
-
-class Member:
-    def __init__(self, name, username, password):
-        self.name = name
-        self.username = username
-        self.password = password
+        # user_list.append(name)
         # print(self.name, self.username)
+    def user_info(self):
+        print(self.name, self.username)
+
 
 m1 = Member("도노반", "Mr.70", 70)
 m2 = Member("스코티", "by", "rapgod")
 m3 = Member("불곰", "콜라", "주세요")
+
+# m1.user_info()
+# # 도노반 Mr.70
+
+# print(user_dict)
+# # {'도노반': '도노반', '스코티': '스코티', '불곰': '불곰'}
