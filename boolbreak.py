5bbe8228769a09ec8817aa4977d0b96f44bb8483
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-27 15:03:29 +0900

    None의 boolean은 False??

---
+++ boolbreak.py
@@ -0,0 +1,24 @@
+# def imgonnabreak(haha):
+#     while 1:
+#         if haha == 'haha':
+#             print("haha")
+#         else:
+#             break
+
+# def gwayeon():
+#     while 1:
+#         print("try")
+#         if not imgonnabreak(haha):
+#             break
+#         else:
+#             print("비상!")
+
+# haha = 'haha'
+
+# gwayeon()
+
+
+# # print(bool(break))
+# # # SyntaxError: invalid syntax
+
+print(bool(None))
