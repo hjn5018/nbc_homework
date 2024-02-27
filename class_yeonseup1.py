4c87270c95883580e443bdddc8e4fd22a59d8819
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-27 13:35:39 +0900

    클래스 연습 조금 간추려보기

---
+++ class_yeonseup1.py
@@ -0,0 +1,34 @@
+class Amugeona:
+    norae = 'shinnaneungeollo'
+# ===============================================================================================
+# print(__dir__)
+# # can't open file 'class_yeonseup1.py': [Errno 2] No such file or directory
+# touch하고 저장 안해서..
+
+# print(amugeona.__dir__)
+# # can't open file 'class_yeonseup1.py': [Errno 2] No such file or directory
+# touch하고 저장 안해서..
+    
+# print(amugeona)
+# # can't open file 'class_yeonseup1.py': [Errno 2] No such file or directory
+# touch하고 저장 안해서..
+    
+# print(amugeona())
+# # can't open file 'class_yeonseup1.py': [Errno 2] No such file or directoryv
+# touch하고 저장 안해서..
+
+# print(__dir__)
+# # NameError: name '__dir__' is not defined
+# ================================================================================================
+# print(dir())
+# # ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'amugeona']
+    
+# print(amugeona.__dir__)
+# # <method '__dir__' of 'object' objects>
+    
+# print(amugeona)
+# <class '__main__.amugeona'>
+    
+# print(amugeona())
+# # <__main__.amugeona object at 0x0000026E276C97F0>
+    
